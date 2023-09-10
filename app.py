46b32791935224ee5a624984d577be436a850dbb
Alonso ASUS <alonsocopete@outlook.es>
2023-09-09 23:15:06 -0500

    feat: linear regression added

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 
 st.title('Modelo de regresión. :chart_with_upwards_trend:')
@@ -25,18 +27,26 @@ with st.sidebar:
         st.write('Selecciona las columnas dependientes e independientes')
         x_col = st.selectbox('Columna independiente', cols)
         y_col = st.selectbox('Columna dependiente', cols)
-        graficar = None
+        entrenar_modelo = None
         if x_col != 'Sin seleccionar' and y_col != 'Sin seleccionar':
-
-            if st.button('Crear gráfica'):
-                graficar = True
+            if st.button('Entrenar modelo'):
+                entrenar_modelo = True
+        if entrenar_modelo is True:
+            df = df.dropna()
+            X = df[x_col].values.reshape(-1, 1)
+            y = df[y_col].values.reshape(-1, 1)
+            model = LinearRegression()
+            model.fit(X, y)
+            y_pred = model.predict(X)
+            st.write('Modelo entrenado')
 
 # Elementos de la página principal
 if df is not None:
     st.write('Preview de los datos')
     st.dataframe(df.head())
-    if graficar is True:
-        progress_text = 'Operation in progress...'
+    graficar_modelo = None
+    if entrenar_modelo is True:
+        progress_text = 'Graficando recta de regresión...'
         my_bar = st.progress(0, text=progress_text)
         for percent_complete in range(100):
             time.sleep(0.05)
@@ -44,7 +54,9 @@ if df is not None:
         fig, ax = plt.subplots(figsize=(4, 4))
         st.write('Gráfica de los datos')
         plt.scatter(df[x_col], df[y_col])
+        plt.plot(df[x_col], y_pred, color='red', label='Regresión lineal')
         plt.title('Gráfica de los datos')
         plt.xlabel(x_col)
         plt.ylabel(y_col)
+        plt.legend()
         st.pyplot(fig)
